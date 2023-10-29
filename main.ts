d8536687ad90fafb18adbcba36d80e9a7fcd6ce5
tytl-code <148522638+tytl-code@users.noreply.github.com>
2023-10-29 11:02:50 +1100

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -166,23 +166,64 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Teleporter, function (sprite, ot
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile19`, function (sprite, location) {
     scene.setBackgroundImage(assets.image`myImage6`)
-    tiles.setCurrentTilemap(tilemap`level51`)
-    scene.cameraFollowSprite(mySprite2)
-    mySprite2.setImage(assets.image`myImage7`)
+    tiles.setCurrentTilemap(tilemap`level28`)
+    Camera = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.decor)
+    Camera.setImage(img`
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+        `)
+    sprites.destroy(Portal)
+    sprites.destroy(mySprite2)
     FinalBoss = 1
-    Boom_Boom = sprites.create(assets.image`myImage9`, SpriteKind.Projectile)
     statusbar = statusbars.create(100, 4, StatusBarKind.Health)
     statusbar.positionDirection(CollisionDirection.Top)
+    Boom_Boom.setPosition(200, 110)
+    scene.cameraFollowSprite(Camera)
     if (FinalBoss == 1) {
         while (FinalBoss == 1) {
             scene.setBackgroundImage(assets.image`myImage10`)
             pause(100)
             scene.setBackgroundImage(assets.image`myImage6`)
             Boom_Boom = sprites.create(assets.image`myImage9`, SpriteKind.Projectile)
+            Boom_Boom.setPosition(200, 110)
             Boom_Boom.follow(mySprite)
             pause(2000)
         }
     }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(Boom_Boom)
 })
 statusbars.onZero(StatusBarKind.Health, function (status) {
     if (Boss2 == 1) {
@@ -384,6 +425,7 @@ let Up = 0
 let ifyfifrirfryfriyfir: Sprite = null
 let Boom_Boom: Sprite = null
 let FinalBoss = 0
+let Camera: Sprite = null
 let Portal: Sprite = null
 let Spin = 0
 let Spinman: Sprite = null
@@ -463,87 +505,19 @@ forever(function () {
 forever(function () {
     if (controller.A.isPressed()) {
         if (Left) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . 1 6 1 . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, -1000, 0)
+            projectile = sprites.createProjectileFromSprite(assets.image`t`, mySprite, -1000, 0)
             music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
             pause(200)
         } else if (Down == 1) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . 1 6 1 . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, 0, 1000)
+            projectile = sprites.createProjectileFromSprite(assets.image`ty`, mySprite, 0, 1000)
             music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
             pause(200)
         } else if (Up == 1) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . 1 6 1 . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, 0, -1000)
+            projectile = sprites.createProjectileFromSprite(assets.image`tyu`, mySprite, 0, -1000)
             music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
             pause(200)
         } else if (Right == 1) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . 1 6 1 . . . . . . . 
-                . . . . . . . 1 . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, 1000, 0)
+            projectile = sprites.createProjectileFromSprite(assets.image`tyui`, mySprite, 1000, 0)
             music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.InBackground)
             pause(200)
         }
@@ -563,7 +537,7 @@ forever(function () {
     music.play(music.stringPlayable("A E F G F E D F ", 90), music.PlaybackMode.UntilDone)
 })
 forever(function () {
-    music.play(music.createSong(hex`0078000408020206001c00010a006400f401640000040000000000000000000000000000000002600000000400012704000800012008000c0001240c001000012010001400012414001800012018001c0001241c002000012020002400012424002800012028002c0001242c003000012030003400012434003800012038003c0001223c004000012409010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8002a0008000900010b10001100010b18001900010b20002100010b28002900010b30003100010b38003900010b`), music.PlaybackMode.UntilDone)
+    music.play(music.createSong(assets.song`song`), music.PlaybackMode.UntilDone)
 })
 forever(function () {
     music.setVolume(255)
